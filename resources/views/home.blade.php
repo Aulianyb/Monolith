2a414d21d4d84f26e9fa1b52e4c7272fc78532b2
Aulia Nadhirah Yasmin Badrulkamal <Aulianybk123@gmail.com>
2023-07-14 16:53:34 +0700

    Make FE Katalog Barang
    Buat FE untuk Katalog Barang

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,21 +1,44 @@
 @extends('layouts.app')
-
 @section('content')
+<head> 
+</head>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+            <h1> 
+                Katalog Barang
+            </h1>
+            <div class="catalog-container"> 
+                <div class="card container catalog-box" style="width:200px;">
+                    <div class="card-body row"> 
+                        <div class="col"> 
+                            <h5 class="card-title">Nama Barang</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">Rp 9000</h6>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">Stok : x</h6>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+                        <button type="button" class="btn btn-primary"> Buy </button>
+                    </div> 
+                </div> 
+                <div class="card container catalog-box" style="width:200px;">
+                    <div class="card-body row"> 
+                        <div class="col"> 
+                            <h5 class="card-title">Nama Barang</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">Rp 9000</h6>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">Stok : x</h6>
+                        </div>
+                        <button type="button" class="btn btn-primary"> Buy </button>
+                    </div> 
+                </div> 
+                <div class="card container catalog-box" style="width:200px;">
+                    <div class="card-body row"> 
+                        <div class="col"> 
+                            <h5 class="card-title">Nama Barang</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">Rp 9000</h6>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">Stok : x</h6>
+                        </div>
+                        <button type="button" class="btn btn-primary"> Buy </button>
+                    </div> 
+                </div> 
             </div>
         </div>
     </div>
